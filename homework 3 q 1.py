413c95f76d1d79535dc5c8975d0a2756c4e43b28
abbyparr <123125559+abbyparr@users.noreply.github.com>
2023-02-06 19:30:56 -0500

    Add files via upload
    I kept having error messages in relation to inputs pop up. I know that my classmates were also having this same issue.

---
+++ homework 3 q 1.py
@@ -0,0 +1,20 @@
+percent == (input('What is your grade percentage?'[0:1])
+            
+if (percent[0.90:0.97])
+    print ("A")
+    
+if(percent[0.80:0.87]):
+    print("B")
+
+if(percent[0.70:0.77]):
+    print("C")
+
+if(percent[0.60:0.67]):
+    print("D")
+
+if(percent[0.0:0.57]):
+    print("F")
+    
+    else:
+        percent != input()
+        print("inappropriate input")
---
+++ homework 3 q 2.py
@@ -0,0 +1,15 @@
+month == int(input('What month?'))
+
+if (month =='Februrary'):
+    print("28")
+    
+if (month == 'April, June, September, November'):
+    print("30")
+    
+if (month == 'Janurary, March, May, July, August, October, December'):
+    print("31")
+
+
+if (int(input() != month):
+    print("inappropriate input")
+    
